10f6812259afc7da04f86063005d9ad4e21ea098
rurico <106768848+ruaneenrico@users.noreply.github.com>
2022-10-26 17:18:36 -0300

    Add files via upload

---
+++ atividade 002/script.js
@@ -0,0 +1,12 @@
+let deuses = ['Aegir','aud','balder'];
+
+deuses.push('loki','odin','frey');
+console.log(deuses);
+
+let number = [5,7,1,8,9]
+number.sort()
+
+console.log(number)
+
+number.reverse()
+console.log(number)
